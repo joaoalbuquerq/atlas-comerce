b9dc6a1dc50a5504dd5c77481be64552965d1ebf
Joao Albuquerque <joaoalbuquerque.dev@gmail.com>
2026-02-18 10:49:48 -0300

    adicionando mapeamento das entidades de pedido

---
+++ pedidos/src/main/java/com/atlascomerce/pedidos/model/Pedido.java
@@ -0,0 +1,50 @@
+package com.atlascomerce.pedidos.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(name = "codigo_cliente", nullable = false)
+    private Long codigoCliente;
+
+    @Column(name = "data_pedido", nullable = false)
+    private LocalDateTime dataPedido;
+
+    @Column(name = "total", precision = 16, scale = 2)
+    private BigDecimal total;
+
+    @Column(name = "chave_pagamento")
+    private String chavePagamento;
+
+    @Column(name = "observacoes")
+    private String observacoes;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+
+    @Column(name = "codigo_rastreio")
+    private String codigoRastreio;
+
+    @Column(name = "url_nf")
+    private String urlNotaFiscal;
+
+
+
+}
---
+++ pedidos/src/main/java/com/atlascomerce/pedidos/model/StatusPedido.java
@@ -0,0 +1,11 @@
+package com.atlascomerce.pedidos.model;
+
+public enum StatusPedido {
+
+    REALIZADO,
+    PAGO,
+    FATURADO,
+    ENVIADO,
+    ERRO_PAGAMENTO,
+    PREPARANDO_ENVIO
+}
